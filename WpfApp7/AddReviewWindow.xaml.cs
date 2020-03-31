fc07afd2acdea344a6dc3b7264397e3bc19c4b68
Naoya <kusuriuri12121@yahoo.co.jp>
2020-03-31 22:18:54 +0900

    プロジェクト ファイルを追加します。

---
+++ WpfApp7/AddReviewWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp7
+{
+    /// <summary>
+    /// AddReviewWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class AddReviewWindow : Window
+    {
+        public AddReviewWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddGameEvaluationToDb()
+        {
+            using(SQLiteConnection conn = new SQLiteConnection(Properties.Resources.ConnectionString))
+            {
+                conn.Open();
+
+                using(DataSet dataSet = new DataSet()) { 
+                    
+                    if(this.GameTitleText.Text != String.Empty)
+                    {
+                        String sql = String.Format("INSERT INTO GAME_EVALUATE (TITLE,RANK) VALUES('{0}','AAA')",
+                            this.GameTitleText.Text,
+                            this.GameEvaluationText);
+
+                        SQLiteDataAdapter dataAdapter = new SQLiteDataAdapter(sql, conn);
+                        dataAdapter.Fill(dataSet);
+                    }
+                    else
+                    {
+                        MessageBox.Show("タイトルなし", caption: "警告ぅ", MessageBoxButton.OK);
+                    }
+
+                }
+
+                conn.Close();
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            AddGameEvaluationToDb();
+            this.Close();
+        }
+    }
+}
---
+++ WpfApp7/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+using System.Data.SQLite;
+
+namespace WpfApp7
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            using (var conn = new SQLiteConnection("Data Source=GameEvalDb.sqlite"))
+            {
+                conn.Open();
+
+                using(var command = conn.CreateCommand())
+                {
+
+                    StringBuilder sb = new StringBuilder();
+                    sb.Append("CREATE TABLE IF NOT EXISTS GAME_EVALUATE(");
+                    sb.Append("TITLE TEXT NOT NULL");
+                    sb.Append(",RANK TEXT NOT NULL");
+                    sb.Append(")");
+
+                    command.CommandText = sb.ToString();
+                    command.ExecuteNonQuery();
+
+                }
+
+                conn.Close();
+            }
+
+
+
+        }
+
+        private void AddReviewBtn_Click(object sender, RoutedEventArgs e)
+        {
+            AddReviewWindow adw = new AddReviewWindow();
+            adw.Show();
+        }
+
+
+
+
+        private void ShowReviewBtn_Click(object sender, RoutedEventArgs e)
+        {
+            ShowGameEvaluationDataGrid();
+        }
+
+
+        private void ShowGameEvaluationDataGrid()
+        {
+            using (var conn = new SQLiteConnection("Data Source=GameEvalDb.sqlite"))
+            {
+                conn.Open();
+
+                using (DataSet dataSet = new DataSet())
+                {
+                    String sql = "SELECT TITLE , RANK FROM GAME_EVALUATE";
+                    SQLiteDataAdapter dataAdapter = new SQLiteDataAdapter(sql, conn);
+                    dataAdapter.Fill(dataSet);
+                    this.ReviewListViewGrid.AutoGenerateColumns = false;
+                    this.ReviewListViewGrid.DataContext = dataSet.Tables[0].DefaultView;
+                }
+                conn.Close();
+            }
+        }
+
+    }
+}
